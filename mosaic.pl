f163200244befa161acc868f082cf15e2a8e6652
Don Smith <don.c.smith@gmail.com>
2013-12-20 15:57:08 -0600

    broken again.  adding support for differing sample and output sizes

--- mosaic.pl
+++ mosaic.pl
@@ -11,14 +11,19 @@ use Carp;
 
 my $tile_set = 'misc';
 my $cell_size = 20;
+my $sample_size = 20;
+my $output_size = 0;
 my $src_file;
 
 GetOptions(
 	'tiles=s' => \$tile_set,
 	'cell=i' => \$cell_size,
+	'sample=i' => \$sample_size,
+	'output=i' => \$output_size,
 	'src=s' => \$src_file
 );
 
+$output_size = $output_size < 1 ? $sample_size : $output_size;
 
 # Disable output buffering
 select((select(STDOUT), $|=1)[0]);
@@ -117,9 +122,8 @@ foreach my $f (@FILES) {
 	if (-f $image_path && $f =~ m/\.(jpe?g)$/gi) {
 		my $tmp_img = GD::Image->newFromJpeg($image_path);
 		my ($tw, $th) = $tmp_img->getBounds();
-		$tiles->{$f} = GD::Image->new($cell_size, $cell_size);
-		
-		$tiles->{$f}->copyResized($tmp_img, 0, 0, 0, 0, $cell_size, $cell_size, $tw ,$th);
+		$tiles->{$f} = GD::Image->new($output_size, $output_size); #$cell_size, $cell_size);
+		$tiles->{$f}->copyResized($tmp_img, 0, 0, 0, 0, $output_size, $output_size, $tw, $th); # $cell_size, $cell_size, $tw ,$th);
 		$tile_avg_color->{$f} = $cache->get($cache_key);
 		if (!$tile_avg_color->{$f}) {
 			my $im = GD::Image->newFromJpeg($image_path);
@@ -136,14 +140,24 @@ print "$image_path\n";
 my $src = GD::Image->newFromJpeg($image_path);
 my ($width, $height) = $src->getBounds();
 
+my $horiz = $width / $sample_size;
+my $vert = $height / $sample_size;
 
-my $gen_file = $gen_path . $src_file;
-my $gen = GD::Image->new($width, $height);
 
+my $gen_file = $gen_path . $tile_set . '-' . $src_file;
+my $gen = GD::Image->new($horiz * $output_size, $vert * $output_size); # $width, $height);
 
-for (my $y = 0; $y < $height; $y += $cell_size) {
-	for (my $x = 0; $x < $width; $x += $cell_size) {
-		my $avg =  avgImg($src, $x, $y, $cell_size, $cell_size);
+#for (my $y = 0; $y < $height; $y += $sample_size) { # $cell_size) {
+#	for (my $x = 0; $x < $width; $x += $sample_size) { # $cell_size) {
+for (my $y_idx = 0; $y_idx < $vert; $y_idx++) { # $cell_size) {
+	for (my $x_idx = 0; $x_idx < $horiz; $x_idx++) { # $cell_size) {
+		
+		my $ix = $x_idx * $sample_size;
+		my $iy = $y_idx * $sample_size;
+		my $ox = $x_idx * $output_size;
+		my $oy = $y_idx * $output_size;
+		
+		my $avg =  avgImg($src, $ix, $iy, $sample_size, $sample_size); #$cell_size, $cell_size);
 		my $closest = 'XXX';
 		my $closest_dist = 100000000000;
 		foreach my $clr (keys %$tile_avg_color) {
@@ -153,8 +167,9 @@ for (my $y = 0; $y < $height; $y += $cell_size) {
 				$closest = $clr;
 			}
 		}
-		
-		$gen->copy($tiles->{$closest}, $x, $y, 0, 0, $cell_size, $cell_size);
+		#$image->copyResized($sourceImage,$dstX,$dstY,$srcX,$srcY,$destW,$destH,$srcW,$srcH)
+		$gen->copyResized($tiles->{$closest}, $ox, $oy, $ix, $iy, $output_size, $output_size, $sample_size, $sample_size); # $cell_size, $cell_size, $tw ,$th);
+		#$gen->copy($tiles->{$closest}, $ix, $iy, 0, 0, $cell_size, $cell_size);
 	}
 }
 
